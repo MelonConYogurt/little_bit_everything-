85d4d5c43b0e6d119068aacdcf6919b9bcfa4557
MelonConYogurt <alejopsornal@gmail.com>
2024-08-20 00:08:55 -0500

    Commit-FrontEnd

--- .venv/src/Eje34.py
+++ .venv/src/Eje34.py
@@ -1,5 +1,5 @@
-class arbol():
-    def __init__(self) -> list:
+class Arbol:
+    def __init__(self):
         self.data = []
         self.padres = []
 
@@ -14,11 +14,11 @@ class arbol():
                 return False
             
             if hijos:
-                if not self.verificar_hijos(hijos):
-                    print(f"Error: Ya el hijo existe en el sistema.")
+                if self.verificar_hijos(hijos):
+                    print(f"Error: Uno o más hijos ya existen en el sistema.")
                     return False
                 else:
-                    self.padres.append({"padre": nombre, "madre": "" })
+                    self.padres.append({"padres": [nombre], "hijos": hijos })
 
             persona = {
                 "id": len(self.data),
@@ -29,56 +29,49 @@ class arbol():
             self.data.append(persona)
     
             print(f"Persona agregada al árbol familiar:\n"
-                f"ID: {id}\n"
+                f"ID: {persona['id']}\n"
                 f"Nombre: {nombre}\n"
                 f"Pareja: {pareja if pareja else 'N/A'}\n"
                 f"Hijos: {', '.join(hijos) if hijos else 'Ninguno'}")
             print()
     
+            return True
+
         except Exception as e:
             print(f"Error al agregar la persona: {e}")
             return False
-        finally:
-            return persona
 
-    def verificar_existencia(self, nombre:str):
+    def verificar_existencia(self, nombre: str) -> bool:
         try:
             for person in self.data:
                 if person["nombre"] == nombre:
                     return True
+            return False
         except Exception as e:
             print(e)
+            return False
 
-    def verificar_pareja(self, nombre:str):
+    def verificar_pareja(self, nombre: str) -> int:
         numero_coincidencias = 0
         try:
-           if self.verificar_existencia(nombre):
-            for person in self.data:
-                if person["pareja"] == nombre:
-                    numero_coincidencias += 1
+            if self.verificar_existencia(nombre):
+                for person in self.data:
+                    if person["pareja"] == nombre:
+                        numero_coincidencias += 1
         except Exception as e:
             print(e)
-        finally:
-            return numero_coincidencias
+        return numero_coincidencias
 
-    def verificar_hijos(self, hijos: list[str]):
+    def verificar_hijos(self, hijos: list[str]) -> bool:
         try:
             for hijo in hijos:
-                if self.verificar_existencia(hijo):
-                    print(f"Error: El hijo '{hijo}' ya existe en el sistema.")
-                    return False
-                else:
-                    self.verificar_padres(hijo):
+                for padre in self.padres:
+                    if hijo in padre["hijos"] and self.verificar_existencia(hijo):
+                        return True
+            return False
         except Exception as e:
             print(e)
-
-
-    def verificar_padres(self, hijo):
-        try:
-
-            pass           
-        except Exception as e:
-            print(e)
+            return False
 
 if __name__ == '__main__':
     pass
