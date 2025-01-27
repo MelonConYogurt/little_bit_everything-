2a1a10e8e2821d3ac8f95243c8c1b8978e3ccf9a
MelonConYogurt <alejopsornal@gmail.com>
2025-01-27 18:10:12 -0500

    commit

---
+++ src/test.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+if __name__ == "__main__":
+    contrasena = get_password_hash("admin")
+    print(contrasena)
+    print("Hola")
